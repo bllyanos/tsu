3f0f99037ceabd9c951553bcc3566979fa9e29cc
Billy Editiano <bllyanos@gmail.com>
2024-03-28 09:52:48 +0700

    add module entry

--- mod.ts
+++ mod.ts
@@ -1,3 +1,8 @@
+/**
+ * This module contains small utility functions.
+ * @module
+ */
+
 /** Check if a value is not null or undefined. */
 export function hasValue<T>(value: T | null | undefined): value is T {
   return value !== null && value !== undefined;
